4587729ae2cf0102a62d9b786158281307390986
Maxon2019 <56646878+Maxon2019@users.noreply.github.com>
2020-03-01 19:25:45 +0300

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -1,12 +1,20 @@
-g = int(input())
+g = 1
+n = int(input())
+p = 0
+i = 0
 
 
-def fib(g):
-    if g == 0:
-        return 1
+def fib(g,p,i, n):
+    i += 1
+    if n == i:
+        return g
     else:
-        g = fib(g+1)
-    return g
+        k = g
+        g = g+p
+        p = k
+        g = fib(g, p,i,n)
+        return g
 
 
-print(fib(g))
+print(fib(g,p,i,n))
+ 
